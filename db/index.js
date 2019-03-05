4d7a8fb3f8782a0bc7fa122321b39de70326503c
Taiwei Tuan <taiweituan@gmail.com>
2019-03-05 16:28:30 -0600

    style

--- db/index.js
+++ db/index.js
@@ -3,17 +3,17 @@ import todoModel from './models/todo';
 
 const env = process.env.NODE_ENV || 'development';
 const db = {};
-
+let sequelize;
 
 if (env !== 'production') {
-  const sequelize = new Sequelize(undefined, undefined, undefined, {
-    dialect: 'sqlite',
-    storage: `${__dirname}/data/dev-todo-api.sqlite`,
-  });
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        dialect: 'sqlite',
+        storage: `${__dirname}/data/dev-todo-api.sqlite`,
+    });
 }
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
 });
 
 db.todo = sequelize.import(todoModel);
