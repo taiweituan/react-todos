e635c5f8b21ccf9e1b416edc8b2ac013bdc9e8eb
Taiwei Tuan <taiweituan@gmail.com>
2019-03-05 16:27:02 -0600

    modified server

--- db/index.js
+++ db/index.js
@@ -3,22 +3,22 @@ import todoModel from './models/todo';
 
 const env = process.env.NODE_ENV || 'development';
 const db = {};
-let sequalize;
 
-if (env === 'production') {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-    });
-} else {
-    sequelize = new Sequelize(undefined, undefined, undefined, {
-        'dialect': 'sqlite',
-        'storage': __dirname + '/data/dev-todo-api.sqlite',
-    });
+
+if (env !== 'production') {
+  const sequelize = new Sequelize(undefined, undefined, undefined, {
+    dialect: 'sqlite',
+    storage: `${__dirname}/data/dev-todo-api.sqlite`,
+  });
 }
 
-db.todo = sequalize.import({todoModel});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+});
 
-db.sequalize = sequalize;
+db.todo = sequelize.import(todoModel);
+
+db.sequalize = sequelize;
 db.Sequelize = Sequelize;
 
 export default db;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,56 @@
-import express from "express";
-// import _ from "lodash";
+import express from 'express';
+import bodyParser from 'body-parser';
+import _ from 'lodash';
+import db from '../db';
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(bodyParser.json());
+
+// GET all todos
+app.get('/todo', (req, res) => {
+    db.sequalize.sync().then((todo) => {
+        res.json(todo);
+    }).catch((e) => {
+        console.log(e);
+        return res.status(500).send();
+    });
+});
+
+// POST /todos
+app.post('/todos', (req, res) => {
+    var body = _.pick(req.body, 'description', 'completed');
+
+    // validation
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    // construct database
+    db.todo.create(body).then(function(todos) {
+        if (todos) {
+            // res.json(todos.toJSON());
+            console.log(todos.toJSON());
+            req.user.addTodo(todos).then(() => {
+                // the todos result is different from database
+                // thus, reload is needed
+                return todos.reload();
+            }).then((todo) => {
+                res.json(todo.toJSON());
+            });
+        } else {
+            console.log('no todo found');
+        }
+    }).catch(function(e) {
+        res.status(400).json(e);
+    });
+});
+
+db.sequelize.sync({
+    force: false
+}).then(function() {
+    app.listen(PORT, function() {
+        console.log('expresss listiening on port: ' + PORT);
+    });
+});
