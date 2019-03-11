9348caaa79b3f037f75fbe45372aaa81807fec5d
Taiwei Tuan <taiweituan@gmail.com>
2019-03-11 01:01:38 -0500

    fixed sqlite sequalize issue

--- db/index.js
+++ db/index.js
@@ -1,38 +1,27 @@
 const path = require('path');
 const Sequelize =  require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const dbPath = path.resolve(__dirname, '/data/test.sqlite');
-// const db = 
+const dbPath = path.resolve(__dirname, '../data/test.sqlite');
+const db = {};
 let sequelize;
 
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
+        operatorsAliases: false
     });
 } else {   
     sequelize = new Sequelize(undefined, undefined, undefined, {
         dialect: 'sqlite',
-        storage: dbPath
+        storage: dbPath,
+        operatorsAliases: false
     });
 }
 
-db.todo = sequelize.define('todo', {
-    description:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate:{
-            len: [1, 250]
-        }
-    },
-    completed: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    }
-});
+db.todo = sequelize.import(`${__dirname}/models/todo.js`);
 
-sequelize.sync();
-// db.sequalize = sequelize;
-// db.Sequelize = Sequelize;
+// sequelize.sync();
+db.sequalize = sequelize;
+db.Sequelize = Sequelize;
 
-// module.exports = db;
+module.exports = db;
