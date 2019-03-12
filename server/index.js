5485004186ea60377d384f63ac2258b596241af9
Taiwei Tuan <taiweituan@gmail.com>
2019-03-12 17:00:43 -0500

    Added delete

--- server/index.js
+++ server/index.js
@@ -10,18 +10,48 @@ app.use(bodyParser.json());
 
 // GET all todos
 app.get('/todos', (req, res) => {
+    console.log('GET Todos');
+    const query = req.query;
+    let where = {};
+
     db.sequalize.sync().then(() => {
-        // TODO
-        return res.json(db.todo);
+        db.todo.findAll({
+            where: where
+        }).then((todo) => {
+            console.log(todo.dataValues);
+            return res.json(todo);
+        });
     }).catch((e) => {
         console.log(e);
         return res.status(500).send();
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    console.log(`GET Todo ID: ${req.params.id}`);
+    const todoId = parseInt(req.params.id, 10);
+    let where = {
+        id: todoId
+    };
+
+    db.todo.findOne({
+        where: where
+    }).then((todo) => {
+        if (todo) {
+            console.log(todo.toJSON());
+            res.json(todo.toJSON());
+        } else {
+            console.log('todo not found');
+            return res.status(404).send();
+        }
+    }).catch((e) => {
+        return res.status(500).json(e);
+    });
+});
+
 // POST /todos
 app.post('/todos', (req, res) => {
-    var body = _.pick(req.body, 'description', 'completed');
+    const body = _.pick(req.body, 'description', 'completed');
 
     // validation
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
@@ -32,7 +62,6 @@ app.post('/todos', (req, res) => {
     // construct database
     db.todo.create(body).then((todos) => {
         if (todos) {
-            // res.json(todos.toJSON());
             console.log(todos.toJSON());
             req.user.addTodo(todos).then(() => {
                 // the todos result is different from database
@@ -49,8 +78,29 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    console.log(`DELETE Todo ID: ${req.params.id}`);
+    const todoId = parseInt(req.params.id, 10);
+    let where = {
+        id: todoId
+    };
+
+    db.todo.destroy({
+        where: where
+    }).then((todo) => {
+        if (todo) {
+            console.log(`DELETED: Todo ID: ${req.params.id}`);
+            res.json({status: 'success'});
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((e) => {
+        return res.status(500).json(e);
+    });
+});
+
 db.sequalize.sync({
-    force: true
+    force: false    // set 'true' to create a new database when server runs
 }).then(function() {
     app.listen(PORT, function() {
         console.log('expresss listiening on port: ' + PORT);
