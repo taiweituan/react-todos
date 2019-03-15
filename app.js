b0f95109d2627d00bc8b1bc68e464dd0fcc53dd4
Taiwei Tuan <taiweituan@gmail.com>
2019-03-15 15:13:38 -0500

    Finish refactoring todo controller

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const _ = require('lodash');
-const env = process.env.NODE_ENV || 'development';
+
 const db = require('./db/index');
 
 /**
@@ -16,81 +15,17 @@ const app = express();
 const PORT = process.env.PORT || 8081;
 app.use(bodyParser.json());
 
-
 // GET all todos
 app.get('/todos', todoController.getTodos);
 
-app.get('/todos/:id', (req, res) => {
-    console.log(`GET Todo ID: ${req.params.id}`);
-    const todoId = parseInt(req.params.id, 10);
-    let where = {
-        id: todoId
-    };
-
-    db.todo.findOne({
-        where: where
-    }).then((todo) => {
-        if (todo) {
-            console.log(todo.toJSON());
-            res.json(todo.toJSON());
-        } else {
-            console.log('todo not found');
-            return res.status(404).send();
-        }
-    }).catch((e) => {
-        return res.status(500).json(e);
-    });
-});
+// GET one todo
+app.get('/todos/:id', todoController.getOneTodo);
 
 // POST /todos
-app.post('/todos', (req, res) => {
-    const body = _.pick(req.body, 'description', 'completed');
+app.post('/todos', todoController.createTodo);
 
-    // validation
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        console.log('Error: Missing either "completed" or "description" field!');
-        return res.status(400).send();
-    }
-
-    // construct database
-    db.todo.create(body).then((todos) => {
-        if (todos) {
-            console.log(todos.toJSON());
-            req.user.addTodo(todos).then(() => {
-                // the todos result is different from database
-                // thus, reload is needed
-                return todos.reload();
-            }).then((todo) => {
-                res.json(todo.toJSON());
-            });
-        } else {
-            console.log('no todo found');
-        }
-    }).catch((e) => {
-        res.status(400).json(e);
-    });
-});
-
-app.delete('/todos/:id', (req, res) => {
-    console.log(`DELETE Todo ID: ${req.params.id}`);
-    const todoId = parseInt(req.params.id, 10);
-    let where = {
-        id: todoId
-    };
-
-    db.todo.destroy({
-        where: where
-    }).then((todo) => {
-        if (todo) {
-            console.log(`DELETED: Todo ID: ${req.params.id}`);
-            res.json({status: 'success'});
-        } else {
-            return res.status(404).send();
-        }
-    }).catch((e) => {
-        return res.status(500).json(e);
-    });
-});
+// DELETE /todos/:id
+app.delete('/todos/:id', todoController.deleteTodo);
 
 
 db.sequalize.sync({
--- controllers/todo.js
+++ controllers/todo.js
@@ -1,19 +1,97 @@
 const db = require('../db/index');
+const _ = require('lodash');
 
-exports.getTodos = (req, res) => {
-    console.log('GET Todos');
-    // const query = req.query;
-    let where = {};
+module.exports = {
+    // GET /todos
+    getTodos: (req, res) => {
+        console.log('GET Todos');
+        // const query = req.query;
+        let where = {};
 
-    db.sequalize.sync().then(() => {
-        db.todo.findAll({
+        db.sequalize.sync().then(() => {
+            db.todo.findAll({
+                where: where
+            }).then((todo) => {
+                console.log(todo.dataValues);
+                return res.json(todo);
+            });
+        }).catch((e) => {
+            console.log(e);
+            return res.status(500).send();
+        });
+    },
+
+    // GET todos/:id
+    getOneTodo: (req, res) => {
+        console.log(`GET Todo ID: ${req.params.id}`);
+        const todoId = parseInt(req.params.id, 10);
+        let where = {
+            id: todoId 
+        };
+
+        db.todo.findOne({
             where: where
         }).then((todo) => {
-            console.log(todo.dataValues);
-            return res.json(todo);
+            if (todo) {
+                console.log(todo.toJSON());
+                res.json(todo.toJSON());
+            } else {
+                console.log('todo not found');
+                return res.status(404).send();
+            }
+        }).catch((e) => {
+            return res.status(500).json(e);
         });
-    }).catch((e) => {
-        console.log(e);
-        return res.status(500).send();
-    });
+    },
+
+    // POST /todos
+    createTodo: (req, res) => {
+        const body = _.pick(req.body, 'description', 'completed');
+    
+        // validation
+        if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+            console.log('Error: Missing either "completed" or "description" field!');
+            return res.status(400).send();
+        }
+    
+        // construct database
+        db.todo.create(body).then((todos) => {
+            if (todos) {
+                console.log(todos.toJSON());
+                req.user.addTodo(todos).then(() => {
+                    // the todos result is different from database
+                    // thus, reload is needed
+                    return todos.reload();
+                }).then((todo) => {
+                    res.json(todo.toJSON());
+                });
+            } else {
+                console.log('no todo found');
+            }
+        }).catch((e) => {
+            res.status(400).json(e);
+        });
+    },
+
+    // DELETE /todos/:id
+    deleteTodo: (req, res) => {
+        console.log(`DELETE Todo ID: ${req.params.id}`);
+        const todoId = parseInt(req.params.id, 10);
+        let where = {
+            id: todoId
+        };
+    
+        db.todo.destroy({
+            where: where
+        }).then((todo) => {
+            if (todo) {
+                console.log(`DELETED: Todo ID: ${req.params.id}`);
+                res.json({status: 'success'});
+            } else {
+                return res.status(404).send();
+            }
+        }).catch((e) => {
+            return res.status(500).json(e);
+        });
+    }
 };
