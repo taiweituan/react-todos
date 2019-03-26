82b88ffc019738d6ff3cdce4d1587535f4ccc042
Taiwei Tuan <taiweituan@gmail.com>
2019-03-26 08:41:14 -0500

    todoform

--- views/components/todos/TodoForm.js
+++ views/components/todos/TodoForm.js
@@ -10,15 +10,46 @@ class TodoForm extends React.Component {
                 </div>
             );
         }
-    }
-    /**
-     * 
-     * @param {*} formProps.input
-     * @param {*} formProps.label
-     *  
-     */
-    // Changed to function becuse 'this' needs to reference to StreamCream class
+    };
+    
+    // Changed to function becuse 'this' needs to reference to class
     renderInput = ({input, label, meta}) => {
-        
+        const className = `${meta.error && meta.touched ? error : ""}`;
+
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} />
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+
+    onSubmit = (formValues) => {
+        this.props.onSubmit(formValues);
     }
-}
+
+    render() {
+        return (
+            <form 
+                className="from"
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+                >
+                <Field name="description" component={this.renderInput} label="Enter Todo" />
+                {/* add checkbox */}
+            </form>
+        );
+    }
+};
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if(!formValues.description) {
+        errors.description = "Description is required";
+    }
+
+    // complete complete 
+
+    return errors;
+};
